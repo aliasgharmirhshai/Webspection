a00a4088e5c03a64efdf3a2493d048b23d10d025
aliasgharmirhshai <lmyrshahy3@gmail.com>
2022-04-11 18:31:17 +0430

    Config List

--- run.py
+++ run.py
@@ -4,30 +4,6 @@ import platform
 import os
 
 if __name__ == '__main__':
-
+    
     while True:
-        config.clear_screen()
-        config.check_version()
-        config.check_module()
-        config.banner()
-        config.main_list()
-
-        commend = input(Fore.GREEN + f'''
-┌──({Fore.LIGHTCYAN_EX + platform.uname()[0] + '@' + platform.uname()[1]}{Fore.GREEN})-[{Fore.LIGHTWHITE_EX + os.getcwd()}{Fore.GREEN}]
-└─$ ''')
-
-        if commend == "0":
-            print(Fore.LIGHTRED_EX + "\nbye ):")
-            exit()
-
-        elif commend == "1":
-            config.clear_screen()
-            config.info_list()
-
-        elif commend == "2":
-            config.clear_screen()
-            config.scan_list()
-
-        elif commend == "3":
-            config.clear_screen()
-            config.attack_list()
+        config.start()
--- settings/config.py
+++ settings/config.py
@@ -2,9 +2,11 @@ import os
 import platform
 from colorama import Fore
 from time import sleep
+import run
 
 def banner():
-    print("Banner\n")
+    RED = '\033[31m'
+    print(RED + "Banner\n")
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -48,23 +50,54 @@ def main_list():
     print(Fore.LIGHTWHITE_EX + "\t[0] EXIT\n")
     sleep(0.1)
 
+# Information Gathering
 def info_list():
+    clear_screen()
     print(Fore.LIGHTBLUE_EX + "\t\tINFORMATION GATHERING\n")
     sleep(0.1)
     print(Fore.LIGHTWHITE_EX + "OPTIONS:\n")
     sleep(0.1)
     print(Fore.LIGHTWHITE_EX + "\t[1] Information Gathering\n")
     sleep(0.1)
-    input()
+    print(Fore.LIGHTWHITE_EX + "\t[0] Back\n")
+    sleep(0.1)
 
+    try:
+        info_command = input(Fore.LIGHTCYAN_EX + f'''
+┌──(Webspection/information-gathring/)
+└─$ {Fore.LIGHTWHITE_EX}''')
+
+        if info_command == "0":
+            start()
+        
+    except EOFError:
+        print(Fore.LIGHTRED_EX + "\nbye ):")
+        exit()
+
+# Scanning
 def scan_list():
     print(Fore.LIGHTBLUE_EX + "\t\tSCANNING\n")
     sleep(0.1)
     print(Fore.LIGHTWHITE_EX + "OPTIONS:\n")
     sleep(0.1)
-    print(Fore.LIGHTWHITE_EX + "\t[2] Scanning\n")
+    print(Fore.LIGHTWHITE_EX + "\t[1] Scanning\n")
+    sleep(0.1)
+    print(Fore.LIGHTWHITE_EX + "\t[0] Back\n")
     sleep(0.1)
 
+    try:
+        scan_command = input(Fore.LIGHTCYAN_EX + f'''
+┌──(Webspection/information-gathring/)
+└─$ {Fore.LIGHTWHITE_EX}''')
+
+        if scan_command == "0":
+            start()
+        
+    except EOFError:
+        print(Fore.LIGHTRED_EX + "\nbye ):")
+        exit()
+
+# Web Attacking
 def attack_list():
     print(Fore.LIGHTWHITE_EX + "\t\t[3] WEB ATTACKING\n")
     sleep(0.1)
@@ -72,4 +105,49 @@ def attack_list():
     sleep(0.1)
     print(Fore.LIGHTWHITE_EX + "\t[3] Web Attacking\n")
     sleep(0.1)
+    print(Fore.LIGHTWHITE_EX + "\t[0] Back\n")
+    sleep(0.1)
 
+    try:
+        attack_command = input(Fore.LIGHTCYAN_EX + f'''
+┌──(Webspection/information-gathring/)
+└─$ {Fore.LIGHTWHITE_EX}''')
+
+        if attack_command == "0":
+            start()
+        
+    except EOFError:
+        print(Fore.LIGHTRED_EX + "\nbye ):")
+        exit()
+
+
+def start():
+        clear_screen()
+        check_version()
+        check_module()
+        banner()
+        main_list()
+
+        try:
+            command = input(Fore.GREEN + f'''
+┌──({Fore.LIGHTCYAN_EX + platform.uname()[0] + '@' + platform.uname()[1]}{Fore.GREEN})-[{Fore.LIGHTWHITE_EX + os.getcwd()}{Fore.GREEN}]
+└─$ ''')
+
+            if command == "0":
+                print(Fore.LIGHTRED_EX + "\nbye ):")
+                exit()
+
+            elif command == "1":
+                info_list()
+
+            elif command == "2":
+                clear_screen()
+                scan_list()
+
+            elif command == "3":
+                clear_screen()
+                attack_list()
+
+        except EOFError:
+            print(Fore.LIGHTRED_EX + "\nbye ):")
+            exit()
